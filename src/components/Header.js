57139cf267d2cf2b0165c9a089ce494cb051b7d8
momin-ctg <you@example.com>
2019-11-27 12:33:26 +0600

    ns

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../footer.css';
+
+function Header() {
+    return (
+        <div className="header">
+            this is header
+        </div>
+    )
+}
+
+export default Header;
