02d45a851883dff13f2cbaae259b94aa6dd66f99
momin-ctg <you@example.com>
2019-11-27 16:17:52 +0600

    nothing special

--- src/Main.js
+++ src/Main.js
@@ -2,27 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function Main() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Scrimba Bootcamp
-        </a>
-      </header>
-      <Footer/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Scrimba Bootcamp
+                </a>
+            </header>
+            <Footer/>
+        </div>
+    );
 }
 
 export default Main;
